0ba9fc60367187f708c98cb263a58fbf53e0b641
anasrazaofficial <anasbukhari4114@gmail.com>
2024-06-13 18:32:20 +0500

    Add images to cloudinary

--- course_3(Udemy-ProJSBackend)/ejsAndCloudinary/app.js
+++ course_3(Udemy-ProJSBackend)/ejsAndCloudinary/app.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const fileUpload = require('express-fileupload')
+const cloudinary = require('cloudinary').v2
 
 const app = express()
+
+cloudinary.config({
+    cloud_name: "dlqjthwo5",
+    api_key: "841316358199787",
+    api_secret: "jD1L2Pd5IuVnCKDuSgPIYAJXbt4"
+})
 
 app.set('view engine', 'ejs')
 
@@ -16,9 +23,38 @@ app.get('/get', (req, res) => {
     // res.status(200).json(req.body) // In postman and in get request, query is treated as body
     res.status(200).json(req.query)   // In browser and in get request, query is treated as query 
 })
-app.post('/post', (req, res) => {
-    console.log(req.files);
-    res.status(200).json(req.body)
+app.post('/post', async (req, res) => {
+
+    // ### Use case for multiple images
+    let imageArray = [], results = []
+
+    if (req.files) {
+        for (let index = 0; index < req.files.sampleFile.length; index++) {
+            let result = await cloudinary.uploader.upload(req.files.sampleFile[index].tempFilePath, {
+                folder: "users"
+            })
+            results.push(result)
+            imageArray.push({
+                public_id: result.public_id,
+                secure_url: result.secure_url
+            })
+        }
+    }
+
+
+    // ### Use case for one image
+    // let file = req.files.sampleFile
+    // let result = await cloudinary.uploader.upload(file.tempFilePath, { folder: 'users' })
+
+
+    let details = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        result: results,
+        images: imageArray
+    }
+    console.log(details);
+    res.status(200).json(details)
 })
 
 app.get('/getform', (req, res) => {
